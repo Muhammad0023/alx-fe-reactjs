20d243f3e2dc2eb3464df7a87f802d5f20a25f56
Muhammad Ali <MUHE098@GMAIL.COM>
2025-08-03 08:26:42 +0300

    Initial commit: GitHub User Search setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>GitHub User Search</h1>
+      <p>This is the starting point of the application.</p>
+    </div>
+  );
+}
+
+export default App;
