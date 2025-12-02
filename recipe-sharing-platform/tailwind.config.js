04510ea9b71b6163933ab7ba17d64589cba99b73
muhammad <muhe098@gmail.com>
2025-12-02 10:05:45 +0300

    Fix Tailwind config and update main.jsx

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // vite main HTML
+    "./public/index.html", // ALX specific requirement
+    "./src/**/*.{js,ts,jsx,tsx}", // all components
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
